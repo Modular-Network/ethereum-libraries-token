7ffb39ecc9511610f0dcc0bf39cf5d393fe4c82e
H <hackdom@majoolr.io>
2018-04-09 19:00:30 -0500

    add-crowdsale-token

--- migrations/2_deploy_test_contracts.js
+++ migrations/2_deploy_test_contracts.js
@@ -1,11 +1,13 @@
 var BasicMathLib = artifacts.require("ethereum-libraries-basic-math/contracts/BasicMathLib.sol");
 var TokenLib = artifacts.require("./TokenLib.sol");
-var TokenLibTestContract = artifacts.require("./TokenLibTestContract");
+var TokenLibTestContract = artifacts.require("./TokenLibTestContract.sol");
+var CrowdsaleToken = artifacts.require("./CrowdsaleToken.sol")
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(BasicMathLib,{overwrite: false});
   deployer.link(BasicMathLib, TokenLib);
   deployer.deploy(TokenLib, {overwrite: false});
+  deployer.link(TokenLib, CrowdsaleToken);
 
   if(network === "development" || network === "coverage"){
     deployer.link(TokenLib, TokenLibTestContract);
